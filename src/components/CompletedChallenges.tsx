3ff6aea4cc92ec2b8bff040f41036a5195e790de
luanrem <luanrem@gmail.com>
2021-02-27 11:46:09 -0300

    ContextAPI implemented

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,10 +1,14 @@
+import { useContext } from 'react';
+import { challengesContext } from '../contexts/ChallengesContext';
 import sytles from '../styles/components/CompletedChallenges.module.css';
 
 export function CompletedChallenges() {
+  const { challengesCompleted } = useContext(challengesContext)
+
   return (
     <div className={sytles.completedChallengesContainer}>
       <span>Desafios Completos</span>
-      <span>5</span>
+      <span>{challengesCompleted}</span>
     </div>
   )
 }
--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,18 +1,24 @@
+import { useContext } from 'react';
 import { createSecureContext } from 'tls';
+import { challengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ExperienceBar.module.css';
 
 export default function ExperienceBar() {
+  const { currentExperience, experienceToNextLevel } = useContext(challengesContext)
+
+  const percentToNextLevel = Math.round((currentExperience * 100) / experienceToNextLevel)
+
   return (
     <header className={styles.experienceBar}>
       <span>0 px</span>
       <div >
-        <div style={{ width: '50%' }} />
+        <div style={{ width: `${percentToNextLevel}%` }} />
 
-        <span className={styles.currentExperience} style={{ left: '50%' }}>
-          300 xp
+        <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%` }}>
+          {currentExperience} xp
         </span>
       </div>
-      <span>600 px</span>
+      <span>{experienceToNextLevel} px</span>
     </header>
   )
 }
