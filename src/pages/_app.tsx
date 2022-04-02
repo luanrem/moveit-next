b018bc42d74aba467c3dae328785b871462ae8fd
luanrem <luanrem@gmail.com>
2022-04-01 22:25:00 -0300

    insert ChallengesProvider properties

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,13 @@
+import "../styles/global.css";
 
-import '../styles/global.css';
-
-import { ChallengesProvider } from '../contexts/ChallengesContext';
+import { ChallengesProvider } from "../contexts/ChallengesContext";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChallengesProvider>
-        <Component {...pageProps} />
+    <ChallengesProvider level={0} currentExperience={0} challengesCompleted={0}>
+      <Component {...pageProps} />
     </ChallengesProvider>
-  )
+  );
 }
 
-export default MyApp
-
+export default MyApp;
